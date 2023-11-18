43e444664d7a640db4f40eea67a2ed164be74998
Wisam <wisam.waad@hotmail.com>
2023-11-18 23:27:38 +0100

    feat: Add resize option

--- touch_the_dot.py
+++ touch_the_dot.py
@@ -6,17 +6,33 @@ import sys
 import random  # Standard library imports first
 import threading
 import time
-from typing import Tuple, Optional, Any
+from typing import List, Tuple, Optional, Any
+from enum import Enum
+from dataclasses import dataclass
 import cv2
 import numpy as np
 import mediapipe as mp
 
-EASY_MODE = 50
-MEDIUM_MODE = 25
-HARD_MODE = 5
 
-DIFFICULTY = {EASY_MODE: "Easy Mode",
-              MEDIUM_MODE: "Medium Mode", HARD_MODE: "Hard Mode"}
+class Modes(Enum):
+    """
+    Enum class with game modes: easy, medium and hard.
+    """
+
+    EASY = 50
+    MEDIUM = 25
+    HARD = 5
+
+
+@ dataclass
+class Shapes:
+    """
+    Data class with sizes of frame and side panel.
+    """
+
+    frame_height = 480
+    side_panel_width = 280
+
 
 COUNTDOWN = 60
 
@@ -48,9 +64,8 @@ class HandDetector:
                     frame, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
         return frame
 
-    @staticmethod
-    def get_index_tip_coordinates(frame: np.ndarray, results: Any) \
-            -> Optional[Tuple[int, int]]:
+    @ staticmethod
+    def get_index_tip_coordinates(frame: np.ndarray, results: Any) -> Optional[Tuple[int, int]]:
         """
         Retrieve the coordinates of the index finger tip from hand landmarks.
         """
@@ -69,14 +84,14 @@ class TouchGame:
     Class representing the Touch the Dot game.
     """
 
-    def __init__(self, difficulty: str = EASY_MODE) -> None:
+    def __init__(self, difficulty: str = Modes.EASY.value) -> None:
         self.difficulty = difficulty
-        self.counter = 0
         self.detector = HandDetector()
         self.circle_found = True
-        self.circle_coords: Optional[Tuple[int, int]] = None
         self.game_over = False
+        self.circle_coords: Optional[Tuple[int, int]] = None
         self.timer = f"{COUNTDOWN}"
+        self.resize_factor = 1
 
     def countdown(self, game_time: int) -> None:
         """
@@ -113,10 +128,55 @@ class TouchGame:
                     return True
         return False
 
+    @staticmethod
+    def add_text(frame: np.ndarray, text: str, position: Tuple[int, int]) -> None:
+        """
+        Write text onto a frame at a certain postion.
+        """
+        # pylint: disable=no-member
+        cv2.putText(frame, text, position, 5, 1.5, (255, 255, 255), 2)
+
+    def find_index_tip(self, frame: np.ndarray) -> List[Tuple[int, int]]:
+        """
+        Find the tip of the index finger.
+        """
+        results = self.detector.process_frame(frame)
+        frame = self.detector.draw_landmarks(frame, results)
+        index_tip = self.detector.get_index_tip_coordinates(frame, results)
+        return index_tip
+
+    def _resize_frame(self, side_panel: np.ndarray, key: int) -> np.ndarray:
+        """
+        Resize frame.
+        """
+        # pylint: disable=no-member
+        if key == ord('1'):
+            self.resize_factor = 1
+            side_panel = cv2.resize(
+                side_panel, (Shapes.side_panel_width, Shapes.frame_height))
+        elif key == ord('2'):
+            self.resize_factor = 1.5
+            side_panel = cv2.resize(
+                side_panel, (Shapes.side_panel_width,
+                             int(Shapes.frame_height * self.resize_factor)))
+        return side_panel
+
+    def _set_difficulty(self, key: int) -> int:
+        """
+        Set game difficulty.
+        """
+        if key == ord('e'):
+            self.difficulty = Modes.EASY.value
+        elif key == ord('m'):
+            self.difficulty = Modes.MEDIUM.value
+        elif key == ord('h'):
+            self.difficulty = Modes.HARD.value
+
     def game_loop(self) -> None:
         """
         The main game loop.
         """
+        counter = 0
         timer_thread = threading.Thread(
             target=self.countdown, args=(COUNTDOWN,))
         timer_thread.start()
@@ -128,7 +188,7 @@ class TouchGame:
             sys.exit()
 
         side_panel = np.full(
-            (480, 270, 3), (0, 0, 0), dtype=np.uint8)
+            (Shapes.frame_height, Shapes.side_panel_width, 3), (0, 0, 0), dtype=np.uint8)
 
         while True:
             ret, frame = cap.read()
@@ -136,13 +196,16 @@ class TouchGame:
                 print("Can't receive frame (stream end?). Exiting ...")
                 break
 
+            if self.resize_factor != 1:
+                frame = cv2.resize(
+                    frame, (int(frame.shape[1] * self.resize_factor),
+                            int(frame.shape[0] * self.resize_factor)))
+
             if self.circle_found:
                 self.circle_coords = self.draw_random_circle(frame)
                 self.circle_found = False
 
-            results = self.detector.process_frame(frame)
-            frame = self.detector.draw_landmarks(frame, results)
-            index_tip = self.detector.get_index_tip_coordinates(frame, results)
+            index_tip = self.find_index_tip(frame)
 
             touch_detected = self.check_touch(index_tip)
 
@@ -151,38 +214,28 @@ class TouchGame:
                 cv2.circle(frame, self.circle_coords, 5, (255, 0, 0), -1)
 
             side_panel[:] = (0, 0, 0)
-            # pylint: disable=no-member
-            cv2.putText(side_panel, f"Time: {self.timer}",
-                        (10, 35), 5, 1.5, (255, 255, 255), 2)
-            # pylint: disable=no-member
-            cv2.putText(side_panel, f"Count: {self.counter}",
-                        (10, 70), 5, 1.5, (255, 255, 255), 2)
-            # pylint: disable=no-member
-            cv2.putText(side_panel, f"{DIFFICULTY[self.difficulty]}",
-                        (5, 460), 5, 1.5, (255, 255, 255), 2)
+            self.add_text(side_panel, f"Time: {self.timer}", (10, 35))
+            self.add_text(side_panel, f"Count: {counter}", (10, 70))
+            self.add_text(
+                side_panel, f"Mode: {Modes(self.difficulty).name}", (10, side_panel.shape[0] - 20))
+
             full_game = np.hstack((frame, side_panel))
-            # pylint: disable=no-member
             cv2.imshow("Frame", full_game)
 
             if touch_detected:
                 self.circle_found = True
-                self.counter += 1
+                counter += 1
 
             if self.game_over:
-                print(f"Final score: {self.counter}")
+                print(f"Final score: {counter}")
                 break
 
             key = cv2.waitKey(1) & 0xFF
             # pylint: disable=no-member
-            if key == ord('e'):
-                self.difficulty = EASY_MODE
-            # pylint: disable=no-member
-            elif key == ord('m'):
-                self.difficulty = MEDIUM_MODE
-            # pylint: disable=no-member
-            elif key == ord('h'):
-                self.difficulty = HARD_MODE
-            # pylint: disable=no-member
+            if key == ord('e') or key == ord('m') or key == ord('h'):
+                self._set_difficulty(key)
+            elif key == ord('1') or key == ord('2'):
+                side_panel = self._resize_frame(side_panel, key)
             elif key == ord('q'):
                 break
 
